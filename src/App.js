f065695906f5158e95d8f8a4283d7e91f11876d1
sunny <sunnysetia93@gmail.com>
2020-12-14 14:51:39 +0530

    profile component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import './App.css';
 
 import Login from './components/Login/Login';
+import Profile from './components/Profile/Profile';
+
+import {getTokenFromUrl} from './spotify';
 
 function App() {
+    const [token, setToken] = React.useState(null);
+
+    useEffect(() => {
+        const token = getTokenFromUrl();
+        setToken(token);
+    }, []);
+
+    console.log('token',token && token.access_token);
     return (
         <div className="App">
-
-            <Login/>
+            {token && token.access_token ? <Profile/> : <Login/>}
         </div>
     );
 }
---
+++ src/components/Profile/Profile.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './Profile.css';
+
+const Profile = () => {
+    return <h1>Profile</h1>
+}
+
+export default Profile;
